950d011abfd0972ceec965d09442841eef299f2c
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2021-09-26 18:00:36 +0530

    adding delete button , save button

--- crome extention/index.js
+++ crome extention/index.js
@@ -1,11 +1,32 @@
 let myLeads = []
+/*myLeads = jsons.stringfy(myLeads)//for converting string into array we can use json.parse()*/
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+tabBtn.addEventListener("click",function(){
+    
+})
+
+deleteBtn.addEventListener("dblclick",function(){
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
 
 inputBtn.addEventListener("click",function(){
     myLeads.push(inputEl.value)
     inputEl.value= ""
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
     renderLeads()
 })
 
