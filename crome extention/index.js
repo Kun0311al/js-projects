1b00c4d7484d39fcdf507bd3f4b786782b56c754
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2021-09-26 15:28:46 +0530

    adding more js and json file

--- crome extention/index.js
+++ crome extention/index.js
@@ -1,4 +1,23 @@
 let myLeads = []
-let inputEl = document.getElementById("input-el")
-let inputBtn = document.getElementById("input-btn")
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
+inputBtn.addEventListener("click",function(){
+    myLeads.push(inputEl.value)
+    inputEl.value= ""
+    renderLeads()
+})
+
+function renderLeads() {
+    let listElements = ""
+    for (let i = 0; i < myLeads.length; i++) {
+    listElements += `
+        <li>
+            <a href="${myLeads[i]}" target="_blank">
+                 ${myLeads[i]} 
+            </a>
+        </li>`
+ }
+ulEl.innerHTML = listElements
+}
